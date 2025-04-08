fa0a55da21796fa8aec0290ba47f81ee98d67ba9
Thai Bao <baothai321@gmail.com>
2025-04-07 23:00:07 -0600

    updated vite?

--- vite.config.js
+++ vite.config.js
@@ -10,8 +10,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path' // You need to import 'resolve' for paths
 
 // https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
@@ -19,4 +21,17 @@ export default defineConfig({
       '/api': 'https://web3-assignment1.onrender.com',
     },
   },
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'), // Make sure this points to the correct path for your index.html
+      },
+    },
+  },
+
+  // Optional: If your assets are located in `src/assets`, you can specify how to handle them
+  assetsInclude: ['src/assets/**', 'src/components/**'], // Ensure your components and assets are bundled
+
+
 })
